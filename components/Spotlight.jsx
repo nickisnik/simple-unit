83d3383ab9369d22d2e544b8f3218e1d45e35c00
Nick <nikita.nikolenko@gmail.com>
2022-07-06 23:37:24 +0100

    Mobile & color invert section

--- components/Spotlight.jsx
+++ components/Spotlight.jsx
@@ -1,21 +1,76 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import {motion} from 'framer-motion'
 
 const Spotlight = () => {
 
+    
     const changeTheme = (text, bg) => {
         document.documentElement.style.setProperty('--text-color', text);
         document.documentElement.style.setProperty('--background', bg);
     }
 
+    const reference = useRef(null);
+
+    
+    const colorInvert = (() => {
+        let intervalFn;
+        const startViewCheck = () => {
+            intervalFn = setInterval(() => {
+                const clientRectObj = reference.current.getBoundingClientRect();
+                const distanceFromTop = clientRectObj.top;
+                const distanceFromBottom = clientRectObj.bottom
+                const elementHeight = clientRectObj.height;
+                if(distanceFromTop < elementHeight / 2 || distanceFromBottom > elementHeight / 2) {
+                    changeTheme('#f9cdcd', '#252422')
+                } 
+                if(distanceFromTop < 0 && - distanceFromTop > elementHeight / 2) {
+                    changeTheme('#252422', '#f4f4f4')
+                }
+                if(distanceFromTop > elementHeight / 2) {
+                    changeTheme('#252422', '#f4f4f4')
+                }
+                
+                
+            }, 100)
+        }
+
+        return {
+            startViewCheck, 
+            stopViewCheck() {
+                clearInterval(intervalFn)
+            }
+        }
+            
+    })()
+    
+    /* useEffect(() => {
+        const int = setInterval(() => {
+
+        })
+        return () => {
+            clearInterval(int)
+        }
+    }) */
+
   return (
-    <section  className='spotlight-wrapper'>
+    <motion.section  className='spotlight-wrapper'
+        ref={reference}
+        onViewportEnter={() => {
+            /* changeTheme('#f9cdcd', '#252422') */
+            colorInvert.startViewCheck()
+            /* checkInView() */
+        }}
+        onViewportLeave={() => {
+            /* changeTheme('#252422', '#f4f4f4') */
+            colorInvert.stopViewCheck()
+            /* clearInterval(intervalFn) */
+        }}>
         <div className='spotlight-textbox'>
                 <span>
                     BASIC/DEPT® HELPS BRANDS ●
                     <motion.span    
-                        onViewportEnter={() => changeTheme('#f9cdcd', '#252422')}
-                        onViewportLeave={() => changeTheme('#252422', '#f4f4f4')}    >
+                        
+                        >
                         CONNECT W/ CULTURE
                     </motion.span>
                 </span>
@@ -24,9 +79,9 @@ const Spotlight = () => {
 
         </div>
         <div className="spotlight-vid-wrapper">
-            <video src="/showcase.mp4" autoPlay muted loop className='showcase-vid' width={700} height={1000}></video>
+            <video src="/showcaseVid.mp4" autoPlay muted loop className='showcase-vid' width={700} height={1000}></video>
         </div>
-    </section>
+    </motion.section>
   )
 }
 
